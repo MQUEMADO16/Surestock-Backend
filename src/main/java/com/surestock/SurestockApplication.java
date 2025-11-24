ed9dfbec20efbde9ec679b2071df7f90d518d2ff
Miguel Quemado <mquemado16@gmail.com>
2025-11-24 00:57:59 -0500

    Set up file structure

---
+++ src/main/java/com/surestock/SurestockApplication.java
@@ -0,0 +1,13 @@
+package com.surestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurestockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurestockApplication.class, args);
+    }
+
+}
